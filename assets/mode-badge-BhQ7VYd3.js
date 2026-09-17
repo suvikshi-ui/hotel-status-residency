780532d119972ad315c27aba7b43c0dbf8bf4ae9
suvikshi-ui <suvikshi@gmail.com>
2026-09-17 08:28:00 +0000

    Publish latest workbook on GitHub Pages
    Live site was still serving an old assets folder. Rebuild index.html
    and the Save cube / Save file bundles so the public site matches main.

---
+++ assets/mode-badge-BhQ7VYd3.js
@@ -0,0 +1,1 @@
+import{tt as e}from"./store-B92vhoes.js";import{r as t}from"./utils-CnPwbm2v.js";import{t as n}from"./badge-j-9gKjtm.js";var r=t(),i={CASH:`cash`,QRS:`qr`,QRPK:`pk`,ONLINE:`online`,BALANCE:`balance`};function a({mode:t}){return(0,r.jsx)(n,{variant:i[t],children:e[t]})}export{a as t};
